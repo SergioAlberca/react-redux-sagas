0ab3b76b0584b1615458beaef06bbed71f58029d
Sergio <s.albercaalberca@gmail.com>
2021-04-02 20:15:55 +0200

    husky added
    husky finish configuration
    
    commmit
    
    test
    
    test successfull

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,6 @@
-import { FetchUpdateUserRequest, IUser } from '../store/users/types';
+import { FetchUpdateUserRequest, IUser } from "../store/users/types";
 
 export const setUsers = (users: IUser[], data: FetchUpdateUserRequest) => {
-  debugger;
   const newData = JSON.parse(JSON.stringify(users));
   const { id, first_name, last_name, email } = data.payload.user;
   const index = newData.findIndex((user: IUser) => user.id === id);
